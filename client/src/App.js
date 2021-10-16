f02e7ddc51d2d482baf7dba0a853b0ab5e09ddc6
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-10-16 17:01:13 +0200

    - Fixed - Welcome message to App.

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-        <main>
-            <h1>Welcome to React!</h1>
-        </main>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+            </header>
+            <main>
+                <h1>Welcome to React!</h1>
+            </main>
+        </div>
+    );
 }
 
 export default App;
