cdb65f47b7f1d695c65308d054fddc15f2e04bed
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-10-19 15:53:15 +0200

    - Added - password field validations to models/constants/user.js

--- models/constants/user.js
+++ models/constants/user.js
@@ -1,5 +1,20 @@
 export const username = {
-    fieldNames : "username",
+    fieldNames: "username",
     minLength: 6,
     maxLength: 30
+}
+
+export const password = {
+    fieldNames: "password",
+    minLength: 8,
+    maxLength: 40,
+    requiresUpperCaseSymbol: true,
+    requiresLowerCaseSymbol: true,
+    requiresDigit: true,
+    requiresNonAlphaNumericSymbol: false
+}
+
+export default {
+    username,
+    password
 }
