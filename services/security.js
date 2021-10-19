04cc7df5d247d22777c64e0a28b963aa1530cb8b
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-10-19 15:54:19 +0200

    - Added - security.js to /services.

--- services/security.js
+++ services/security.js
@@ -1,3 +1,50 @@
+import crypto from "crypto";
+import securityConfigs from "../configs/security.js";
+
+export function generateSaltSync() {
+    return crypto.randomBytes(securityConfigs.passwordSaltLength).toString(securityConfigs.encoding);
+}
+
+export function generateSalt() {
+    return new Promise(((resolve, reject) => {
+        crypto.randomBytes(securityConfigs.passwordSaltLength, (err, salt) => {
+            if (err) {
+                reject(err);
+            }
+
+            resolve(salt.toString(securityConfigs.encoding));
+        });
+    }));
+}
+
+export function generateHashSync(salt, password) {
+    return crypto.pbkdf2Sync(
+        password,
+        salt,
+        securityConfigs.iterationsCount,
+        securityConfigs.passwordHashLength,
+        securityConfigs.encoding);
+}
+
+export function generateHash(salt, password) {
+    return new Promise((resolve, reject) => {
+        crypto.pbkdf2(
+            password,
+            salt,
+            securityConfigs.iterationsCount,
+            securityConfigs.passwordHashLength,
+            securityConfigs.encoding,
+            (err, hash) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve(hash);
+            }
+        )
+    });
+}
+
 const defaultHasAllRoles = false;
 
 export function isAuthenticated(roles, hasAllRoles = defaultHasAllRoles) {
@@ -29,5 +76,9 @@ export function isAuthenticated(roles, hasAllRoles = defaultHasAllRoles) {
 }
 
 export default {
+    generateSaltSync,
+    generateSalt,
+    generateHashSync,
+    generateHash,
     isAuthenticated
 }
