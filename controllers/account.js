673f763ba18f8a07bcc5dba87e8b36c3073b4205
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-10-21 14:53:16 +0200

    - Implemented - registerPost and loginPost methods.

--- controllers/account.js
+++ controllers/account.js
@@ -1,9 +1,38 @@
-function registerPost(req, res) {
-    res.status(200).send("Register POST");
+import userService from "../services/user.js";
+import {signToken, setAuthenticationCookie} from "../services/security.js";
+
+async function registerPost(req, res) {
+    const model = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    try {
+        const user = await userService.createUser(model);
+        res.status(200).send(user);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 }
 
-function loginPost(req, res) {
-    res.status(200).send("Login POST");
+async function loginPost(req, res) {
+    const model = {
+        identifier: req.body.identifier,
+        password: req.body.password
+    }
+
+    try {
+        const user = await userService.checkUser(model);
+        const token = await signToken(user);
+        setAuthenticationCookie(res, token);
+
+        delete user["id"];
+        res.status(200).send(user);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(err);
+    }
 }
 
 function logoutPost(req, res) {
