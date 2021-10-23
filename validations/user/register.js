3ac588ae96b0f6e12a5055252eb9a7586b4cfcc0
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-10-23 11:57:12 +0200

    - Refactored - register model validations.

--- validations/user/register.js
+++ validations/user/register.js
@@ -1,5 +1,10 @@
-import {usernameValidations} from "./fields/username.js";
+import {username} from "./fields/username.js";
+import {email} from "./fields/email.js";
+import {confirmPassword, password} from "./fields/password.js";
 
 export default [
-    ...usernameValidations()
+    username("username"),
+    email("email"),
+    password("password"),
+    confirmPassword("confirmPassword")
 ]
