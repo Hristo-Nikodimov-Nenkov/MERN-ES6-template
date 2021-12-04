ca916f5afdd5835622b558aaa420e9bc2b87b42a
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-12-04 20:45:43 +0100

    - Fixed - wrong secure value of cookies depending on NODE_ENV

--- configs/security.js
+++ configs/security.js
@@ -1,30 +1,35 @@
-import {port} from "./application.js";
+import { port } from "./application.js";
 
 export const defaultEncoding = "base64";
-export const encoding = process.env.SECURITY_ENCODING
-in ["ascii", "utf8", "utf16le", "ucs2", "base64", "binary", "hex"]
-   ? process.env.SECURITY_ENCODING
-   : defaultEncoding;
+export const encoding =
+   process.env.SECURITY_ENCODING in
+   ["ascii", "utf8", "utf16le", "ucs2", "base64", "binary", "hex"]
+      ? process.env.SECURITY_ENCODING
+      : defaultEncoding;
 
 export const defaultPasswordSaltLength = 16;
-export const passwordSaltLength = process.env.PASSWORD_SALT_LENGTH || defaultPasswordSaltLength;
+export const passwordSaltLength =
+   process.env.PASSWORD_SALT_LENGTH || defaultPasswordSaltLength;
 
 export const defaultPasswordHashLength = 64;
-export const passwordHashLength = process.env.PASSWORD_HASH_LENGTH || defaultPasswordHashLength;
+export const passwordHashLength =
+   process.env.PASSWORD_HASH_LENGTH || defaultPasswordHashLength;
 
 export const defaultIterationsCount = 15000;
-export const iterationsCount = process.env.PASSWORD_HASH_ITERATIONS || defaultIterationsCount;
+export const iterationsCount =
+   process.env.PASSWORD_HASH_ITERATIONS || defaultIterationsCount;
 
 export const defaultHashingAlgorithm = "sha512";
-export const hashingAlgorithm = process.env.PASSWORD_HASHING_ALGORITHM || defaultHashingAlgorithm;
+export const hashingAlgorithm =
+   process.env.PASSWORD_HASHING_ALGORITHM || defaultHashingAlgorithm;
 
 export const passwordConfigs = {
    encoding,
    passwordSaltLength,
    passwordHashLength,
    iterationsCount,
-   hashingAlgorithm
-}
+   hashingAlgorithm,
+};
 
 export const defaultTokenSecret = "My JWT Signing Secret";
 export const tokenSecret = process.env.JWT_SECRET || defaultTokenSecret;
@@ -43,15 +48,16 @@ export const tokenConfigs = {
    options: {
       issuer: tokenIssuer,
       audience: tokenAudience,
-      expiresIn: tokenExpiresIn
-   }
-}
+      expiresIn: tokenExpiresIn,
+   },
+};
 
 export const defaultCookieSecret = "My Cookies Signing Secret";
 export const cookieSecret = process.env.COOKIE_SECRET || defaultCookieSecret;
 
 export const defaultAuthenticationCookieName = "Authentication";
-export const authenticationCookieName = process.env.COOKIE_NAME || defaultAuthenticationCookieName;
+export const authenticationCookieName =
+   process.env.COOKIE_NAME || defaultAuthenticationCookieName;
 
 export const cookieConfigs = {
    authenticationCookieName,
@@ -59,9 +65,9 @@ export const cookieConfigs = {
    options: {
       httpOnly: true,
       signed: true,
-      secure: process.env.NODE_ENV !== "development"
-   }
-}
+      secure: process.env.NODE_ENV === "production",
+   },
+};
 
 export const defaultConfigs = {
    defaultEncoding,
@@ -70,12 +76,12 @@ export const defaultConfigs = {
    defaultIterationsCount,
    defaultHashingAlgorithm,
    defaultTokenSecret,
-   defaultCookieSecret
-}
+   defaultCookieSecret,
+};
 
 export default {
    defaultConfigs,
    passwordConfigs,
    tokenConfigs,
-   cookieConfigs
-}
+   cookieConfigs,
+};
