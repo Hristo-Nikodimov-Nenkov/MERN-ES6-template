66da1450bddec8d6e9d34b460bea8003ca976c3f
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-03-28 17:42:29 +0200

    - Changed - middlewares index exports. - Changed - app uses named exports.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 
-import database from "./middlewares/database.js";
-import httpsRedirect from "./middlewares/httpsRedirect.js";
-import cookieParser from "./middlewares/cookieParser.js";
-import bodyParser from "./middlewares/bodyParser.js";
-import staticFiles from "./middlewares/staticFiles.js";
-import authentication from "./middlewares/authentication.js";
-import routes from "./middlewares/routes.js";
-import server from "./middlewares/server.js";
+import {
+   authentication,
+   bodyParser,
+   cookieParser,
+   database,
+   httpsRedirect,
+   routes,
+   server,
+   staticFiles
+} from "./middlewares";
 
 const app = express();
 
