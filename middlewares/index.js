b7660175176f6784b8ea3bb8366acddb8f4c5575
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-03-28 17:46:03 +0200

    - Changed - default to named exports.

---
+++ middlewares/index.js
@@ -0,0 +1,8 @@
+export {default as database} from "./database.js";
+export {default as httpsRedirect} from "./httpsRedirect.js";
+export {default as cookieParser} from "./cookieParser.js";
+export {default as bodyParser} from "./bodyParser.js";
+export {default as staticFiles} from "./staticFiles.js";
+export {default as authentication} from "./authentication.js";
+export {default as routes} from "./routes.js";
+export {default as server} from "./server.js";
