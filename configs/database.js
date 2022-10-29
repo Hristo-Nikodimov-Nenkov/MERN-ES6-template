82cd11447a82b6e0f66b145631d514e6bc58a441
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-10-29 09:34:30 +0200

    - Changed - localhost to 127.0.0.1 in development connection string.

--- configs/database.js
+++ configs/database.js
@@ -1,5 +1,5 @@
 export const developmentDatabaseName = "my-database";
-export const developmentConnectionString = `mongodb://localhost/${developmentDatabaseName}`;
+export const developmentConnectionString = `mongodb://127.0.0.1/${developmentDatabaseName}`;
 export const connectionString =
    process.env.DB_CONNECTION_STRING || developmentConnectionString;
 
